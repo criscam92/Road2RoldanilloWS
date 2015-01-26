ceec15eba44c9cd177cdd8e699217c1287a85450
CRISTIAN <CRISTIAN@CRISTIAN-PC.home>
2015-01-26 13:46:52 -0500

    sha1

--- src/java/r2r/persistencia/facades/UsuarioFacade.java
+++ src/java/r2r/persistencia/facades/UsuarioFacade.java
@@ -5,7 +5,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import r2r.persistencia.entidades.Usuario;
-import r2r.seguridad.Encrypt;
 
 @Stateless
 public class UsuarioFacade extends AbstractFacade<Usuario> {
@@ -25,10 +24,9 @@ public class UsuarioFacade extends AbstractFacade<Usuario> {
     public Usuario login(String userName, String password) {
         Usuario usuario = null;
         try {
-//            System.out.println("CONTRASEÑA: " + Encrypt.getStringMessageDigest(password));
-            Query query = getEntityManager().createQuery("SELECT u FROM Usuario u WHERE u.usuario = :usuario AND u.contrasena = :contrasena");
+            Query query = getEntityManager().createNamedQuery("Usuario.findByUsuarioAndContrasena");
             query.setParameter("usuario", userName);
-            query.setParameter("contrasena", Encrypt.getStringMessageDigest(password));
+            query.setParameter("contrasena", password);
             usuario = (Usuario) query.getSingleResult();
         } catch (Exception e) {
             System.out.println("\n======================= ERROR CONSULTANDO EL USUARIO Y CONTRASENA ======================");
@@ -47,7 +45,7 @@ public class UsuarioFacade extends AbstractFacade<Usuario> {
 
             if (user != null) {
                 if (usuario.getId() != null) {
-                    result = user.getId().equals(usuario.getId());
+                    result = !user.getId().equals(usuario.getId());
                 } else {
                     result = true;
                 }
